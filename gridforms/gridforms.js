3fdfcfca228085ca75f096c3c28ebe1ffe724422
Kumail Hunaid <kumailht@gmail.com>
2013-08-28 20:29:08 +0400

    gridforms.js to focus fields; GA tracking code; Copy update; Tweet and github star buttons;

---
+++ gridforms/gridforms.js
@@ -0,0 +1,47 @@
+//
+//  Grid Forms
+//  Copyright (c) 2013 Kumail Hunaid
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+$(function() {
+	var GridForms = {
+		el: {
+			fieldsContainers: $('[data-field-span]'),
+			focusableFields: $('input, textarea, select', '[data-field-span]')
+		},
+		init: function() {
+			this.focusField(this.el.focusableFields.filter(':focus'));
+			this.events();
+		},
+		focusField: function(currentField) {
+			this.el.fieldsContainers.removeClass('focus');
+			currentField.closest('[data-field-span]').addClass('focus');
+		},
+		events: function() {
+			var that = this;
+			that.el.focusableFields.focus(function() {
+				that.focusField($(this));
+			});
+		}
+	};
+
+	GridForms.init();
+});
