296aa85a599fec23b28cae3b06a785fd7edcee21
Bradley Bain <brad_bain@live.com>
2013-10-22 18:06:49 -0500

    Refactoring...

--- gridforms/gridforms.js
+++ gridforms/gridforms.js
@@ -22,79 +22,75 @@
 //
 
 $(function() {
-	var GridForms = {
-		el: {
-			fieldsRows : $('[data-row-span]'),
-			fieldsContainers: $('[data-field-span]'),
-			focusableFields: $('input, textarea, select', '[data-field-span]')
-		},
-		init: function() {
-			this.focusField(this.el.focusableFields.filter(':focus'));
-			this.events();
-		},
-		focusField: function(currentField) {
-			currentField.closest('[data-field-span]').addClass('focus');
-		},
-		removeFieldFocus: function() {
-			this.el.fieldsContainers.removeClass('focus');
-		},
-		events: function() {
-			var that = this;
-			that.equalizeFieldHeights();
-			that.el.fieldsContainers.click(function() {
-				$(this).find('input, textarea, select').focus();
-			});
-			that.el.focusableFields.focus(function() {
-				that.focusField($(this));
-			});
-			that.el.focusableFields.blur(function() {
-				that.removeFieldFocus();
-			});
-			$(window).resize(function() {
-				that.equalizeFieldHeights();
-			});
-			
-		},
-		equalizeFieldHeights: function() {
-			//reset any forced sizing for the resize event
-			this.el.fieldsContainers.css("height", "auto");
+    var GridForms = {
+        el: {
+            fieldsRows: $('[data-row-span]'),
+            fieldsContainers: $('[data-field-span]'),
+            focusableFields: $('input, textarea, select', '[data-field-span]'),
+            window: $(window)
+        },
+        init: function() {
+            this.focusField(this.el.focusableFields.filter(':focus'));
+            this.equalizeFieldHeights();
+            this.events();
+        },
+        focusField: function(currentField) {
+            currentField.closest('[data-field-span]').addClass('focus');
+        },
+        removeFieldFocus: function() {
+            this.el.fieldsContainers.removeClass('focus');
+        },
+        events: function() {
+            var that = this;
+            that.el.fieldsContainers.click(function() {
+                $(this).find('input, textarea, select').focus();
+            });
+            that.el.focusableFields.focus(function() {
+                that.focusField($(this));
+            });
+            that.el.focusableFields.blur(function() {
+                that.removeFieldFocus();
+            });
+            that.el.window.resize(function() {
+                that.equalizeFieldHeights();
+            });
 
-			//get the rows
-			var rows = this.el.fieldsRows;
-			var fields = this.el.fieldsContainers;
+        },
+        equalizeFieldHeights: function() {
+            this.el.fieldsContainers.css("height", "auto");
 
-			//make sure that the fields aren't stacked
-			if(this.getTotalFieldsWidth(rows) <= rows.first().width()) {
-				rows.each(function() {
-					//get the height of the row (thus the tallest element's height)
-					var row = $(this);
-					var rowHeight = row.height();
+            var fieldsRows = this.el.fieldsRows;
+            var fieldsContainers = this.el.fieldsContainers;
 
-					//set the height for each field in the row...
-					row.find(fields).height(rowHeight);
-				});
-			}
-		},
+            // Make sure that the fields aren't stacked
+            if (!this.areFieldsStacked()) {
+                fieldsRows.each(function() {
+                    // Get the height of the row (thus the tallest element's height)
+                    var fieldRow = $(this);
+                    var rowHeight = fieldRow.height();
 
-		getTotalFieldsWidth : function(rows) {
-			//get the first row 
-			//which does not only contain one field 
-			var firstRow = rows
-				.filter(function() {
-					return $(this).attr("data-row-span") !== "1";
-				})
-				.first();
+                    // Set the height for each field in the row...
+                    fieldRow.find(fieldsContainers).height(rowHeight);
+                });
+            }
+        },
+        areFieldsStacked: function() {
+            // Get the first row 
+            // which does not only contain one field 
+            var firstRow = this.el.fieldsRows
+                .not('[data-row-span="1"]')
+                .first();
 
-			//get to the total width 
-			//of each field witin the row
-			var totalWidth = 0;
-			firstRow.children().each(function() {
-				totalWidth += $(this).width();
-			});
-			return totalWidth;
-			
-		}
-	};
+            // Get to the total width 
+            // of each field witin the row
+            var totalWidth = 0;
+            firstRow.children().each(function() {
+                totalWidth += $(this).width();
+            });
 
-	GridForms.init();
+            // Determine whether fields are stacked or not
+            return firstRow.width() <= totalWidth;
+        }
+    };
+    GridForms.init();
 });
